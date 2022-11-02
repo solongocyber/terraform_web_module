994483e767b6c17c692163be437576a5e89bae55
EC2 Default User <ec2-user@ip-172-31-33-174.us-east-2.compute.internal>
2022-11-02 22:51:29 +0000

    qa

---
+++ qa/main.tf
@@ -0,0 +1,33 @@
+module "vpc" {
+    source = "/home/ec2-user/terraform_web_module/modules/vpc/vpc_v3"
+
+env = "qa"
+vpc_cidr = "10.0.0.0/16"
+public_subnet_cidr = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+private_subnet_cidr = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+cidr_block = "0.0.0.0/0"
+az = ["us-east-2a", "us-east-2b", "us-east-2c"]
+region = "us-east-2"
+stage = "nonprod"
+project = "project"
+
+
+}
+
+module "alb" {
+    source = "/home/ec2-user/terraform_web_module/modules/alb/aws_alb_v2"
+    vpc_id = module.vpc.vpc_id
+    public_subnet = module.vpc.public_subnet[*]
+    env = "qa"
+    region = "us-east-2"
+    
+    
+}
+
+module "auto_scaling" {
+  source = "/home/ec2-user/terraform_web_module/modules/asg_v3"
+  alb_tg = module.alb.alb_tg
+  vpc_id = module.vpc.vpc_id
+  ami = "ami-0f924dc71d44d23e2"
+  public_subnet = module.vpc.public_subnet[*]
+}
